808d1c9380282703c6839c02ec12b3fda5d9286b
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-29 23:18:08 +0200

    writing class and html template for user subscription

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../common/services/connexion/auth.service'
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +11,45 @@ import { AuthService } from '../common/services/connexion/auth.service'
 
 export class RegisterComponent implements OnInit {
 
+  formRegister!: FormGroup;
+  confirmPasswordError: string = "";
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.formRegister = this.formBuilder.group({
+      email: this.formBuilder.control(""),
+      username: this.formBuilder.control(""),
+      password: this.formBuilder.control(""),
+      confirmPassword: this.formBuilder.control("")
+    })
+  }
+
+  handleRegister() {
+    let email = this.formRegister.value.email
+    let username = this.formRegister.value.username
+    let password = this.formRegister.value.password
+    let confirmPassword = this.formRegister.value.confirmPassword
+
+    if (password == confirmPassword) {
+
+      this.confirmPasswordError = "";
+
+      this.authService.register(email, username, password).subscribe({
+        next: data => {
+          console.log(data);
+
+          this.authService.loadProfile(data);
+          this.router.navigateByUrl("/ngr-home");
+        },
+
+        error: err => {
+          console.log(err);
+        }
+      })
+    } else {
+      this.confirmPasswordError = "Les mots de passe sont différents"
+    }
 
   }
 }
