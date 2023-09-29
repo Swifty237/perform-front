2a11d6882f774fd429fc670d55b1802cc1fde61b
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-29 23:25:17 +0200

    Deleting old script

--- src/app/common/services/connexion/user.service.ts
+++ src/app/common/services/connexion/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:3002/performmma/test/';
 
 @Injectable({
   providedIn: 'root',
@@ -10,19 +9,4 @@ const API_URL = 'http://localhost:3002/performmma/test/';
 export class UserService {
   constructor(private http: HttpClient) { }
 
-  getPublicContent(): Observable<any> {
-    return this.http.get(API_URL + 'all', { responseType: 'text' });
-  }
-
-  getUserBoard(): Observable<any> {
-    return this.http.get(API_URL + 'user', { responseType: 'text' });
-  }
-
-  getModeratorBoard(): Observable<any> {
-    return this.http.get(API_URL + 'mod', { responseType: 'text' });
-  }
-
-  getAdminBoard(): Observable<any> {
-    return this.http.get(API_URL + 'admin', { responseType: 'text' });
-  }
 }
