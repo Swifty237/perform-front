d3ae994f1f06ea16e68343c44836189fb53346ee
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-10-03 22:36:30 +0200

    changing http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     backNodeUrl: 'https://perform-back-nodejs.vercel.app',
-    backSpringUrl: "http://35.180.120.190"
+    backSpringUrl: "https://35.180.120.190"
 };
 
 //ng build --configuration=production
