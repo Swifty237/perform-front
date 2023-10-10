caccc77e1eba966a92a3313506c373694066f822
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-10-10 10:41:17 +0200

    adding bootstrap files and modifying prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     backNodeUrl: 'https://perform-back-nodejs.vercel.app',
-    backSpringUrl: "http://perfmmaspringapp.eu-west-3.elasticbeanstalk.com:5000"
+    backSpringUrl: "http://perfmmaspringapp.eu-west-3.elasticbeanstalk.com"
 };
 
 //ng build --configuration=production
