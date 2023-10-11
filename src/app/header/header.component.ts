76f32451113b5cddb55cb57e1f7566fbb118a63c
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-10-11 05:08:43 +0200

    responsive design home and header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../common/services/spring-services/auth.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +10,14 @@ import { Router } from '@angular/router';
 })
 
 export class HeaderComponent implements OnInit {
+  searchForm!: FormGroup
 
-  constructor(public authService: AuthService, private router: Router) { }
+  constructor(public authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-
+    this.searchForm = this.formBuilder.group({
+      searchInput: this.formBuilder.control("")
+    })
   }
 
   handleLogout() {
