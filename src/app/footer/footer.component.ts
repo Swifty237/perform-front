54e9b62746d4f946d55af3c82c4de9b94af18869
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-10-13 12:25:02 +0200

    updating responsive design

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent {
 
+  @Input() opened!: boolean
 }
