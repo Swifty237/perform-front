f1a8c7c48704751bcfed72325e2639cb4167fc3b
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-11-01 08:11:20 +0100

    update commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     backNodeUrl: 'https://perform-back-nodejs.vercel.app',
-    backSpringUrl: "https://perfmma.backspring.link/"
+    backSpringUrl: "http://perfmmaspringapp.eu-west-3.elasticbeanstalk.com/"
 };
 
 //ng build --configuration=production
